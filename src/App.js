1d191b7436135b66967a35b60cd1ad1dabee656a
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-05-27 11:07:07 +0300

    Пример использования useState
    (cherry picked from commit 24d3d53cdf75960db256b323a35788dcda59ecdc)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item +1)
+
+  const decreaseItem = () => setItem(item-1)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+         <div>
+             <div>Item: {item}</div>
+             <button onClick={incrementItem}>Увеличить</button>
+             <button onClick={decreaseItem}>Уменьшить</button>
+         </div>
       </header>
     </div>
   );
