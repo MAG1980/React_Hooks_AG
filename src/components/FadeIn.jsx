f99669c4923f59d5b37a96c88a62a6b3fe8cd155
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2023-06-01 11:48:12 +0300

    Хук useFadeIn использован повторно для плавного появления нескольких компонентов.

--- src/components/FadeIn.jsx
+++ src/components/FadeIn.jsx
@@ -2,13 +2,14 @@ import {useFadeIn} from "../hooks/useFadeIn";
 
 export const FadeIn = () => {
     // const {elementWithFadeIn, style} = useFadeIn()
-    const elementWithFadeIn = useFadeIn()
-    console.log(elementWithFadeIn)
-    const {ref, style} = elementWithFadeIn
+    const h3WithFadeIn = useFadeIn()
+    const pWithFadeIn = useFadeIn()
     return (
         <>
             {/*<h3 ref={ref} style={style}>Fade In</h3>*/}
-            <h3 {...elementWithFadeIn}>Fade In</h3>
+            <h3 {...h3WithFadeIn}>Fade In h3</h3>
+            <p {...pWithFadeIn}>Fade In p</p>
+
         </>
     )
 }
